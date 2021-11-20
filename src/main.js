f02894719a80d20d0a66670bff2072862bcb285f
rustin <1543153730@qq.com>
2021-11-20 20:57:59 +0800

    version 1.0

--- src/main.js
+++ src/main.js
@@ -1,10 +1,54 @@
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+    { logo: "V", url: "https://www.vuejs.org/" },
+    { logo: "D", url: "https://developer.mozilla.org" },
+];
+const simplifyUrl = (url) => {
+    return url
+        .replace("https://", "")
+        .replace("http://", "")
+        .replace("www.", "")
+        .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+
+const render = () => {
+    $(".webclt").find("li:not(#addwebclt)").remove();
+    console.log(1);
+    hashMap.forEach((node, index) => {
+        const $li = $(`
+                <li>
+                    <a href="${node.url}">
+                        <svg class="icon">
+                            <use xlink:href="#icon-16gl-${node.logo}"></use>
+                        </svg>
+                        <span>${simplifyUrl(node.url)}</span>
+                    </a>
+                    <div class="close">
+                <svg class="iconclose">
+                <use xlink:href="#icon-16gl-cross"></use>
+                </svg>
+                </div>
+                </li>
+      `).insertBefore($("#addwebclt"));
+        $li.on("click", ".close", (e) => {
+            e.stopPropagation(); // 阻止冒泡
+            hashMap.splice(index, 1);
+            render();
+            const string = JSON.stringify(hashMap);
+            localStorage.setItem("x", string);
+        });
+    });
+};
+
+render();
+
 dayjs.locale("zh-cn");
 setInterval(() => {
     $("#rlTime").text(dayjs().format("YYYY年MM月DD日HH时mm分ss秒 dddd"));
 }, 1000);
 
 function footerPosition() {
-    console.log("1");
     $(".globefooter").removeClass("fixed-bottom");
 
     let contentHeight = document.body.scrollHeight, //网页正文全文高度
@@ -17,3 +61,53 @@ function footerPosition() {
 }
 footerPosition();
 $(window).resize(footerPosition);
+
+$("#addwebclt").on("click", function () {
+    window.scroll(0, 0);
+    stop();
+    $("#addmodal").removeClass("modaldisabled");
+});
+
+$("#cancel").on("click", function () {
+    move();
+    $("#addweburl").val("");
+    $("#addmodal").addClass("modaldisabled");
+});
+
+$("#confirm").on("click", function () {
+    let url = $("#addweburl").val();
+    if (url.indexOf("http") !== 0) {
+        url = "https://" + url;
+    }
+    hashMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        url: url,
+    });
+    render();
+    const string = JSON.stringify(hashMap);
+    localStorage.setItem("x", string);
+    $("#cancel").click();
+});
+function stop() {
+    let mo = function (e) {
+        passive: false;
+    };
+    document.body.style.overflow = "hidden";
+    document.addEventListener("touchmove", mo, false); //禁止页面滑动
+}
+//取消滚动限制
+function move() {
+    let mo = function (e) {
+        passive: false;
+    };
+    document.body.style.overflow = ""; //出现滚动条
+    document.removeEventListener("touchmove", mo, false);
+}
+$(document).on("keypress", (e) => {
+    const { key } = e;
+    console.log(key);
+    //实现添加时回车即确认功能
+    if (key === "Enter" && !$("#addmodal").hasClass("modaldisabled")) {
+        $("#confirm").click();
+    }
+});
