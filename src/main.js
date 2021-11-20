198648a33131c85ba3bd72e0e59ac2328af950f4
rustin <1543153730@qq.com>
2021-11-20 23:58:28 +0800

    version 1.0

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-const x = localStorage.getItem("x");
-const xObject = JSON.parse(x);
+footerPosition();
+function footerPosition() {
+    $(".globefooter").removeClass("fixed-bottom");
+
+    let contentHeight = document.body.scrollHeight, //网页正文全文高度
+        winHeight = window.innerHeight; //可视窗口高度，不包括浏览器顶部工具栏
+
+    if (contentHeight <= winHeight) {
+        //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
+        $(".globefooter").addClass("fixed-bottom");
+    }
+}
+$(window).resize(footerPosition);
+
+const data = localStorage.getItem("data");
+const xObject = JSON.parse(data);
 const hashMap = xObject || [
     { logo: "V", url: "https://www.vuejs.org/" },
-    { logo: "D", url: "https://developer.mozilla.org" },
+    { logo: "W", url: "https://wangdoc.com/" },
+    { logo: "J", url: "https://www.jquery123.com/" },
 ];
 const simplifyUrl = (url) => {
     return url
@@ -30,13 +45,14 @@ const render = () => {
                 </div>
                 </li>
       `).insertBefore($("#addwebclt"));
+        footerPosition();
         $li.on("click", ".close", (e) => {
             e.stopPropagation(); // 阻止冒泡
             hashMap.splice(index, 1);
             render();
-            footerPosition();
+            window.scrollTo(e.pageX + e.clientX, e.pageY + e.clientY);
             const string = JSON.stringify(hashMap);
-            localStorage.setItem("x", string);
+            localStorage.setItem("data", string);
         });
     });
 };
@@ -48,22 +64,13 @@ setInterval(() => {
     $("#rlTime").text(dayjs().format("YYYY年MM月DD日HH时mm分ss秒 dddd"));
 }, 1000);
 
-function footerPosition() {
-    $(".globefooter").removeClass("fixed-bottom");
-
-    let contentHeight = document.body.scrollHeight, //网页正文全文高度
-        winHeight = window.innerHeight; //可视窗口高度，不包括浏览器顶部工具栏
-
-    if (contentHeight <= winHeight) {
-        //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
-        $(".globefooter").addClass("fixed-bottom");
-    }
-}
-footerPosition();
-$(window).resize(footerPosition);
-
 $("#addwebclt").on("click", function () {
-    window.scroll(0, 0);
+    //计算cover的高度
+    $(".cover").height(
+        window.innerHeight - document.body.scrollHeight >= 0
+            ? window.innerHeight
+            : document.body.scrollHeight
+    );
     stop();
     $("#addmodal").removeClass("modaldisabled");
     $("#addweburl").focus();
@@ -73,6 +80,7 @@ $("#cancel").on("click", function () {
     move();
     $("#addweburl").val("");
     $("#addmodal").addClass("modaldisabled");
+    window.scroll(0, document.body.scrollHeight);
 });
 
 $("#confirm").on("click", function () {
@@ -85,9 +93,8 @@ $("#confirm").on("click", function () {
         url: url,
     });
     render();
-    footerPosition();
     const string = JSON.stringify(hashMap);
-    localStorage.setItem("x", string);
+    localStorage.setItem("data", string);
     $("#cancel").click();
 });
 function stop() {
@@ -105,6 +112,7 @@ function move() {
     document.body.style.overflow = ""; //出现滚动条
     document.removeEventListener("touchmove", mo, false);
 }
+
 $(document).on("keypress", (e) => {
     const { key } = e;
     console.log(key);
